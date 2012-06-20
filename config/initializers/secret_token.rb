3268bac11b45242fc6cffe606b60a6f529cc2fb7
Arian Ryan <arian@beenverified.com>
2012-06-20 14:37:06 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Happymusician::Application.config.secret_token = '4f7d3ed10d7775c0021c9fa0bc455f9cfd786026d46a466c9466727fa9c6bb2ef72400826b87636b473c7b9513a19bef3ada19d49f6d78b6da5b8f4352e4bc31'
